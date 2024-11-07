c943f30ae9c6962891a56c8aa1931734d3abc367
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2024-11-07 20:29:29 +0200

    Add project files.

---
+++ Heartbeats/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Heartbeats.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
