72c7a8a8273a7b76aca466ee8652a0652b61af4b
Mohamed Hazem <100438594+MohamedHazem5@users.noreply.github.com>
2024-11-14 00:44:51 +0200

    Compelete contact page

--- Heartbeats/Controllers/HomeController.cs
+++ Heartbeats/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Heartbeats.Controllers
             return View();
         }
 
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
