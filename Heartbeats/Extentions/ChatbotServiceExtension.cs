c6fdad1ed5eb6bdddb003b16ee37b74b52c0cd49
Moamen Mostafa <mmoamen270@gmail.com>
2024-11-15 23:46:13 +0200

    Add chatbot hosted service

--- Heartbeats/Extentions/ChatbotServiceExtension.cs
+++ Heartbeats/Extentions/ChatbotServiceExtension.cs
@@ -1,47 +1,18 @@
-﻿using System.Diagnostics;
+﻿using Heartbeats.Infrastructure.Interfaces;
+using Heartbeats.Infrastructure.Managers;
+using Heartbeats.Infrastructure.Service;
+using System.Diagnostics;
 
-namespace Heartbeats.Extentions
+namespace Heartbeats.Extensions
 {
     public static class ChatbotServiceExtension
     {
-        public static void StartChatbot(this IServiceCollection services)
+        public static IServiceCollection AddChatbotServices(this IServiceCollection services)
         {
-            // Get the path to the folder where the program.cs file is located
-            var basePath = AppContext.BaseDirectory;
-            Console.WriteLine(basePath);
-
-            // Navigate to the Chatbot folder relative to program.cs
-            var chatbotFolderPath = Path.Combine(basePath, "..", "..", "..", "Chatbot");
-            Console.WriteLine(chatbotFolderPath);
-            // Setting up the shell command to activate the virtual environment and start the chatbot
-            var startInfo = new ProcessStartInfo
-            {
-                FileName = "cmd.exe",
-                Arguments = $"/c \"cd /d {chatbotFolderPath} && .\\venv\\Scripts\\activate && chainlit run .\\app.py -w -h\"",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                CreateNoWindow = true
-            };
-
-            try
-            {
-                var process = new Process { StartInfo = startInfo };
-                process.Start();
-
-                Console.WriteLine($"Started Python chatbot with PID {process.Id}");
-
-                // Optional: read output or error streams if needed
-                process.OutputDataReceived += (sender, e) => Console.WriteLine(e.Data);
-                process.ErrorDataReceived += (sender, e) => Console.WriteLine("Error: " + e.Data);
-
-                process.BeginOutputReadLine();
-                process.BeginErrorReadLine();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to start Python chatbot: {ex.Message}");
-            }
+            services.AddSingleton<IProcessExecutor, ProcessExecutor>();
+            services.AddSingleton<IVirtualEnvironmentManager, PythonVirtualEnvironmentManager>();
+            services.AddHostedService<ChatbotService>();
+            return services;
         }
     }
 }
