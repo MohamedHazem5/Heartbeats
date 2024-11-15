2801fda1d65ca93f0482f817073b8b6e59c5c33d
Moamen Mostafa <mmoamen270@gmail.com>
2024-11-15 23:44:45 +0200

    add DTO records for Processes & Commands Request/Response

---
+++ Heartbeats/Infrastructure/Records/ProcessRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Heartbeats.Infrastructure.Records
+{
+    public record ProcessRequest(
+         string Command,
+         string WorkingDirectory,
+         bool CreateNoWindow = false,
+         bool RedirectOutput = true
+    );
+}
---
+++ Heartbeats/Infrastructure/Records/ProcessResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Heartbeats.Infrastructure.Records
+{
+    public record ProcessResult(
+        int ExitCode,
+        string Output,
+        string Error,
+        bool Success
+    );
+}
